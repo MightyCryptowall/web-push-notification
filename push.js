39e23453b64d1b627f40613442b84f8b3d8a2de9
Ye Thiha <mightycryptowall@gmail.com>
2021-08-22 00:25:25 +0630

    fixed some err

--- push.js
+++ push.js
@@ -11,6 +11,9 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:developer@yethiha.com', vapidKeys.publicKey, vapidKeys.privateKey);
 
-let sub = {};
+let sub = {
+    "endpoint":"https://updates.push.services.mozilla.com/wpush/v2/gAAAAABhI…twZcUDrlWce0rwPO0GdUYGgk__jPwm52Nt5Z56hN4XDPxsWPBXQtUyyWyNrg",
+    "keys":{"auth":"0Z_gRf0LYvVsvXIZsz5xBQ","p256dh":"BLuc0OjcOjiwpfW3KTfnQNyK1YiWgK7An83EmuaVNlHUNd4f_XZq60gNf-xiTbJM2qzbruqO1Ma5aK15tDzz1fk"}
+};
 
 push.sendNotification(sub, 'test message')
